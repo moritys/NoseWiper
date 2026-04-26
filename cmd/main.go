7a455527559b646053bde745b1f5190539fa5325
masha <moriko.tys@gmail.com>
2026-04-26 18:55:59 +0300

    test ver + state

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,3 @@
-// 5902172202:AAHewEIuOCNWpnyfWyVkiY1o7i8RlJrXFKU
 package main
 
 import (
@@ -7,8 +6,15 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+type UserData struct {
+	State string
+	Name  string
+}
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI("5902172202:AAHewEIuOCNWpnyfWyVkiY1o7i8RlJrXFKU")
+	users := make(map[int64]*UserData)
+
+	bot, err := tgbotapi.NewBotAPI("8664604930:AAHIlnf3I1wsJzEfSAM3V0d9w0H6cBjVVJY")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -24,8 +30,50 @@ func main() {
 
 	for update := range updates {
 		if update.Message != nil {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет!")
+			text := update.Message.Text
+			chatID := update.Message.Chat.ID
+			user, exists := users[chatID]
+			if !exists {
+				user = &UserData{}
+				users[chatID] = user
+			}
+
+			if text == "/start" {
+				user.State = "waiting_name"
+
+				msg := tgbotapi.NewMessage(chatID, "Как тебя зовут?")
+				bot.Send(msg)
+				continue
+			}
+
+			if user.State == "waiting_name" {
+				user.Name = text
+				user.State = "waiting_age"
+
+				msg := tgbotapi.NewMessage(chatID, "Сколько тебе лет?")
+				bot.Send(msg)
+				continue
+			}
+
+			if user.State == "waiting_age" {
+				msg := tgbotapi.NewMessage(chatID, "Тебя зовут "+user.Name+", тебе "+text)
+				bot.Send(msg)
+
+				delete(users, chatID)
+				continue
+			}
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ты написал: "+update.Message.Text)
 			bot.Send(msg)
+		}
+		if update.CallbackQuery != nil {
+			data := update.CallbackQuery.Data
+
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Ты нажал: "+data)
+			bot.Send(msg)
+
+			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, "")
+			bot.Request(callback)
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/moritys/wisechoisbotgo/cmd
 
 go 1.26.2
 
-require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // direct
